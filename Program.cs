6a8d8bc19c08ab1d00f41c2dff1961f898468d04
etniesz <jaime-.g@hotmail.cl>
2023-10-17 09:41:52 -0300

    uploading project

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// Challenge 1
+bool amProgrammer = true;
+float Age = 27.9f;
+List<string> Names = new List<string>();
+Names.Add("Monica");
+Dictionary<string, string> MyDictionary = new Dictionary<string, string>();
+MyDictionary.Add("Hello", "0");
+MyDictionary.Add("Hi there", "0");
+// This is a tricky one! Hint: look up what a char is in C#
+string MyName = "MyName";
+
+// Challenge 2
+List<int> Numbers = new List<int>() {2,3,6,7,1,5};
+for(int i = Numbers.Count-1; i > 0; i--)
+{
+    Console.WriteLine(Numbers[i]);
+}
+
+// // Challenge 3
+List<int> MoreNumbers = new List<int>() {12,7,10,-3,9};
+foreach(int i in MoreNumbers)
+{
+     Console.WriteLine(i);
+}
+
+// // Challenge 4
+List<int> EvenMoreNumbers = new List<int> {3,6,9,12,14};
+for (int i = 0; i < EvenMoreNumbers.Count; i++)
+{
+    if (EvenMoreNumbers[i] % 3 == 0){
+        EvenMoreNumbers[i] = 0;
+    }
+}
+
+// Challenge 5
+// What can we learn from this error message?
+//En C#, las cadenas son inmutables, lo que significa que se puede cambiar caracteres individuales dentro de una cadena utilizando un índice como si fuera un arreglo.
+// string MyString = "superduberawesome";
+// MyString[7] = "p";
+string MyString = "superduberawesome";
+MyString = MyString.Substring(0, 7) + "p" + MyString.Substring(8);
+
+// // Challenge 6
+// // Hint: some bugs don't come with error messages
+Random rand = new Random();
+int randomNum = rand.Next(13);
+if(randomNum == 12)
+{
+    Console.WriteLine("Hello");
+}
